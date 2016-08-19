09c60563406d70ac1eb8151edeaa93d700a51153
zerol <m@yanzl.net>
2016-08-19 23:14:26 +0800

    创建表模型

--- azeroth/app_runner.py
+++ azeroth/app_runner.py
@@ -5,7 +5,13 @@ from azeroth.config import config
 
 mysql_db = SQLAlchemy()
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(config)
+    mysql_db.init_app(app)
+    return app
+
+app = create_app()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=config.TEST)
