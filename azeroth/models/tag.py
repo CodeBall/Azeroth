043046ab171a35712ac242130f615792acfc27db
zerol <m@yanzl.net>
2016-08-20 22:22:43 +0800

    修改tag模型，创建数据表

--- azeroth/models/tag.py
+++ azeroth/models/tag.py
@@ -1,14 +1,18 @@
 from azeroth.app_runner import mysql_db as db
 
+from sqlalchemy.dialects.mysql import TINYINT
+
 
 class Tag(db.Model):
     __tablename__ = 'tag'
     id = db.Column(db.Integer, primary_key=True,)
     name = db.Column(db.String(100), index=True, default='default', nullable=False)
     nick_name = db.Column(db.String(100), index=True, default='default')
+    use_group = db.Column(TINYINT, default=True)
     about = db.Column(db.String(255))
 
-    def __init__(self, name, nickname, about = None):
+    def __init__(self, name, nickname, use_group=True, about=None):
         self.name = name
         self.nick_name = nickname
+        self.use_group = use_group
         self.about = about
